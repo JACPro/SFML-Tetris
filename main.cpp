b7c4e4ae5c1a07c895b27e0bc6225a351801e28c
JACPro <48246716+JACPro@users.noreply.github.com>
2025-01-14 15:04:53 +0000

    Add new graphics, reposition elements on screen to fit new textures
    Signed-off-by: JACPro <48246716+JACPro@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@
 const int GRID_ROWS = 20;
 const int GRID_COLS = 10;
 
-const int SCREEN_WIDTH = 320;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 800;
 
-const int TILE_WIDTH = 18; // the width (pixels) of a single tile
+const int TILE_WIDTH = 30; // the width (pixels) of a single tile
 
 const float DEFAULT_DELAY = 0.3f;
 
@@ -19,14 +19,9 @@ int field[GRID_ROWS][GRID_COLS];
 sf::Vector2i currentTetromino[4], tempTetromino[4], nextTetromino[4];
 
 // Next tetromino preview
-const float nextTetrominoBoxPadding = 5.0f;
-const sf::Vector2f nextTetrominoBoxPosition(250.0f, 20.0f);
-sf::RectangleShape nextTetrominoBg(sf::Vector2f(
-	TILE_WIDTH * 2 + (2 * nextTetrominoBoxPadding),
-	TILE_WIDTH * 4 + (2 * nextTetrominoBoxPadding))
-);
-sf::RectangleShape nextTetrominoLabelBg(nextTetrominoBg);
-sf::Text nextTetrominoLabel;
+const float nextTetrominoBoxPadding = 15.0f;
+const sf::Vector2f nextTetrominoBoxPosition(400.0f, 100.0f);
+
  
 int tetrominos[7][4] = {
 	1, 3, 5, 7, // I
@@ -57,17 +52,27 @@ int main() {
 	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML TETRIS");
 
 	// Load Resources
-	sf::Texture tiles, background, frame;
-	tiles.loadFromFile("Resources/Textures/tiles.png");
-	background.loadFromFile("Resources/Textures/background.png");
-	frame.loadFromFile("Resources/Textures/frame.png");
+	sf::Texture tiles, background, grid, frame, nextTileFrame;
+	tiles.loadFromFile("Resources/Textures/Tiles.png");
+	background.loadFromFile("Resources/Textures/Background.png");
+	background.setRepeated(true);
+	grid.loadFromFile("Resources/Textures/Grid.png");
+	frame.loadFromFile("Resources/Textures/Frame.png");
+	nextTileFrame.loadFromFile("Resources/Textures/NextTileFrame.png");
 	sf::Font font;
 	if (!font.loadFromFile("Resources/Fonts/Arial.ttf")) {
 		return 0;
 	}
 
-	sf::Sprite tileSprite(tiles), backgroundSprite(background), frameSprite(frame);
+	sf::Sprite tileSprite(tiles), backgroundSprite(background), gridSprite(grid), nextTileGridSprite(grid), 
+		frameSprite(frame), nextTileFrameSprite(nextTileFrame);
 
+	backgroundSprite.setTextureRect({0, 0, 600, 800});
+	gridSprite.setPosition(50, 100);
+	nextTileGridSprite.setTextureRect({ 0, 0, 90, 150});
+	nextTileGridSprite.setPosition(nextTetrominoBoxPosition);
+	nextTileFrameSprite.setPosition(nextTileGridSprite.getPosition().x -28, nextTileGridSprite.getPosition().y -28);
+	
 	// Initialize field 2D array
 	for (int i = 0; i < GRID_ROWS; i++) {
 		for (int j = 0; j < GRID_COLS; j++) {
@@ -100,31 +105,6 @@ int main() {
 		nextTetromino[i].x = tetrominos[n][i] % 2;
 		nextTetromino[i].y = tetrominos[n][i] / 2;
 	}
-
-	// Next tetromino preview 
-	nextTetrominoLabel.setFont(font);
-	nextTetrominoLabel.setString("NEXT");
-	nextTetrominoLabel.setCharacterSize(18);
-	nextTetrominoLabel.setFillColor(sf::Color::Red);
-	nextTetrominoLabel.setStyle(sf::Text::Bold);
-	nextTetrominoLabel.setPosition(nextTetrominoBoxPosition);
-	
-	sf::FloatRect textLocalBounds = nextTetrominoLabel.getLocalBounds();
-	nextTetrominoLabelBg.setSize(sf::Vector2f(
-		textLocalBounds.width + 2 * nextTetrominoBoxPadding,
-		textLocalBounds.height + 2 * nextTetrominoBoxPadding
-	));
-	nextTetrominoLabelBg.setPosition(
-		nextTetrominoLabel.getPosition().x + textLocalBounds.left - nextTetrominoBoxPadding,
-		nextTetrominoLabel.getPosition().y + textLocalBounds.top - nextTetrominoBoxPadding
-	);
-
-	nextTetrominoBg.setPosition(
-		nextTetrominoLabelBg.getGlobalBounds ().left,
-		nextTetrominoLabelBg.getGlobalBounds().top + nextTetrominoLabelBg.getGlobalBounds().height + nextTetrominoBoxPadding
-	);
-	nextTetrominoBg.setSize(sf::Vector2f(nextTetrominoLabelBg.getGlobalBounds().width, nextTetrominoBg.getGlobalBounds().height));
-	nextTetrominoBg.setFillColor(sf::Color::White);
 
 	// Input 
 	std::unordered_map<sf::Keyboard::Key, KeyHandler> keyHandlers;
@@ -226,6 +206,7 @@ int main() {
 		// Draw
 		window.clear(sf::Color::White);
 		window.draw(backgroundSprite);
+		window.draw(gridSprite);
 
 		// Grid
 		for (int i = 0; i < GRID_ROWS; i++) {
@@ -233,16 +214,15 @@ int main() {
 				if (field[i][j] != -1) {
 					tileSprite.setTextureRect(sf::IntRect(field[i][j] * TILE_WIDTH, 0, TILE_WIDTH, TILE_WIDTH));
 					tileSprite.setPosition(j * TILE_WIDTH, i * TILE_WIDTH);
-					tileSprite.move(28, 31);
+					tileSprite.move(50, 100);
 					window.draw(tileSprite);
 				}
 			}
 		}
 
 		// Next Tetromino
-		window.draw(nextTetrominoLabelBg);
-		window.draw(nextTetrominoLabel);
-		window.draw(nextTetrominoBg);
+		window.draw(nextTileGridSprite);
+		window.draw(nextTileFrameSprite);
 
 		// If no tile square is in first row or col, need offset to center
 		bool topRow = false, leftCol = false, bottomRow = false;
@@ -258,11 +238,11 @@ int main() {
 			}
 		}
 
-		float nextTetrominoBoxHorPadding = nextTetrominoBg.getGlobalBounds().width - 2 * TILE_WIDTH;
+		float nextTetrominoBoxHorPadding = nextTileGridSprite.getGlobalBounds().width - 2 * TILE_WIDTH;
 		nextTetrominoBoxHorPadding /= 2;
 
-		float xOffset = nextTetrominoBg.getPosition().x + nextTetrominoBoxHorPadding;
-		float yOffset = nextTetrominoBg.getPosition().y + nextTetrominoBoxPadding;
+		float xOffset = nextTileGridSprite.getPosition().x + nextTetrominoBoxHorPadding;
+		float yOffset = nextTileGridSprite.getPosition().y + nextTetrominoBoxPadding;
 		if (!leftCol) { // I shape
 			xOffset -= TILE_WIDTH / 2;
 		}
@@ -283,7 +263,7 @@ int main() {
 		for (int i = 0; i < 4; i++) {
 			tileSprite.setTextureRect(sf::IntRect(colourNum * TILE_WIDTH, 0, TILE_WIDTH, TILE_WIDTH));
 			tileSprite.setPosition(currentTetromino[i].x * TILE_WIDTH, currentTetromino[i].y * TILE_WIDTH);
-			tileSprite.move(28, 31);
+			tileSprite.move(50, 100);
 			window.draw(tileSprite);
 		}
 
